d589cdf503ba9616b3c5a78b95c335ff9b51bf40
Sam Whitlock <scw0035@uah.edu>
2024-10-05 11:56:20 -0500

    Fixed collision checking

--- Preliminary2DEngine/Assets/Constants.cs
+++ Preliminary2DEngine/Assets/Constants.cs
@@ -7,7 +7,13 @@ public static class Constants {
 
 
     // Yay, the gravitational constant of the earth
-    public static readonly Vector2 g = new Vector2(0, -9.807f);
+    public static readonly Vector2 g = Vector2.zero;//new Vector2(0, -9.807f);
+
+    // Yay, the fundamental gravitational constant
+    public static readonly float G = 6.67430f * pow(10, -11);
+
+    // The air density (kg/m^3)
+    public static readonly float AIR_DENSITY = 1.225f;
 
     // The size of the screen
     // (0, 0) is in the middle of the screen
--- Preliminary2DEngine/Assets/Main.cs
+++ Preliminary2DEngine/Assets/Main.cs
@@ -19,6 +19,7 @@ public class Main : MonoBehaviour {
     // The gameObject that they want to use as the sprite
     public GameObject particlePicture;
 
+    // TODO Change this to a List instead of an array
     // The list of particles to use
     private particle[] particles;
 
@@ -38,11 +39,12 @@ public class Main : MonoBehaviour {
         for (int i = 0; i < numberOfParticles; i++) {
 
             // Making another circle and using its transform for our particle
-            particles[i] = new particle(Instantiate(particlePicture).transform, 0.1f, 0.1f, new Vector2(Random.Range(-9f, 9f), 0), Vector2.zero);
+            particles[i] = new particle(Instantiate(particlePicture).transform, 0.1f, 0.1f, new Vector2(Random.Range(-9f, 9f), Random.Range(-4f, 4f)), new Vector2(Random.Range(-10f, 10f), Random.Range(-10f, 10f)));
 
             // Giving each a different force
             particles[i].force(new Vector2(i * 2, i));
 
+            // Making it so that each collision isn't elastic   
             particles[i].restitution = 0.75f;
 
         }
@@ -53,12 +55,18 @@ public class Main : MonoBehaviour {
     void Update() {
 
         // Updating the location of each one in the particles thing
-        for (int i = 0; i < numberOfParticles; i++) {
-            particles[i].updateLocation();
+        for (int i = 0; i < particles.Length; i++) {
+            particles[i].update();
         }
 
+        // TODO Adjust this to use multithreading
         // Checking to see if they are colliding
         particle.fixCollisions(particles);
+
+        // Adding gravity
+        for (int i = 0; i < particles.Length; i++) {
+            particle.addGravity(particles[i], particles);
+        }
 
         // Making a force towards the mouse
         if (Input.GetButtonDown("Fire1") && !down) {
@@ -73,7 +81,7 @@ public class Main : MonoBehaviour {
 
                 // We just want to add together the mouse position and the position of the particle, then normalize it 
                 // so that it doesn't have a crazy amount of force
-                Vector3 updatedMousePos = (Camera.main.ScreenToWorldPoint(mousePos) - new Vector3(particles[i].shownTransform.position.x, particles[i].shownTransform.position.y, 0)).normalized * 100;
+                Vector3 updatedMousePos = (Camera.main.ScreenToWorldPoint(mousePos) - new Vector3(particles[i].shownTransform.position.x, particles[i].shownTransform.position.y, 0)).normalized * 500;
 
                 // Applying the force
                 particles[i].force(updatedMousePos);
@@ -83,14 +91,74 @@ public class Main : MonoBehaviour {
         } else {
             down = false;
         }
+
+        // Making sure that the number of particles is not negative
+        numberOfParticles = Mathf.Max(numberOfParticles, 0);
         
+        // Checking to see if we need to get rid of some circles or add more
+        if (numberOfParticles > particles.Length) {
+
+            // The new array
+            particle[] newParticles = new particle[numberOfParticles];
+
+            // Duplicating the particles array into a bigger array
+            System.Array.Copy(particles, newParticles, particles.Length);
+
+            // Looping to add particles
+            for (int i = particles.Length; i < newParticles.Length; i++) {
+
+                // Making another circle and using its transform for our particle
+                newParticles[i] = new particle(Instantiate(particlePicture).transform, 0.1f, 0.1f, new Vector2(Random.Range(-9f, 9f), Random.Range(-4f, 4f)), new Vector2(Random.Range(-10f, 10f), Random.Range(-10f, 10f)));
+
+                // Giving each a different force
+                newParticles[i].force(new Vector2(i * 2, i));
+
+                // Making it so that each collision isn't elastic   
+                newParticles[i].restitution = 0.75f;
+
+            }
+
+            // Resetting the particles to particles
+            particles = newParticles;
+
+        } else if (numberOfParticles < particles.Length) {
+
+            Debug.Log("Removing...");
+
+            // The new length (to ensure nothing weird happens if it goes fast)
+            int newLength = numberOfParticles;
+
+            // Destroying the extra particles
+            for (int i = particles.Length - 1; i > newLength; i--) {
+                Debug.Log("Looping...");
+
+                // Check for null before trying to destroy
+                if (particles[i] != null && particles[i].shownTransform != null && particles[i].shownTransform.parent != null) {
+                Debug.Log($"Destroying particle at index {i}: {particles[i].shownTransform.parent.gameObject.name}");
+                Destroy(particles[i].shownTransform.parent.gameObject);
+                particles[i] = null;
+                }
+
+            }
+
+            // Create a new smaller array to hold the remaining particles
+            particle[] newParticles = new particle[newLength];
+
+            // Duplicating the particles array into a smaller array
+            System.Array.Copy(particles, newParticles, newLength);
+
+            // Saving the new array
+            particles = newParticles;
+
+        }
+
 
     }
 }
 
 
 // The class that I'm going to have for my particle
-class particle {
+public class particle {
 
     // IVs
     public float mass;
@@ -103,6 +171,9 @@ class particle {
 
     // This is the coefficient of restitution. Basically, it is a bounciness percentage
     public float restitution;
+
+    // The coefficient of drag. This helps figure out air resistance
+    public float dragCoeff; 
  
     // This is the transform of the particle object that it is referencing
     public UnityEngine.Transform shownTransform;
@@ -126,6 +197,7 @@ class particle {
 
         // Defaulting things 
         this.restitution = 1;
+        this.dragCoeff = 0.47f; // 0.47 for a sphere according to wikipedia
 
         // Adjusting the scale
         this.shownTransform.localScale = new Vector2(this.radius*2, this.radius*2); 
@@ -142,6 +214,7 @@ class particle {
 
         // Defaulting things 
         this.restitution = 1;
+        this.dragCoeff = 0.47f; // 0.47 for a sphere according to wikipedia
 
         // Adjusting the scale
         this.shownTransform.localScale = new Vector2(this.radius*2, this.radius*2);
@@ -157,6 +230,7 @@ class particle {
 
         // Defaulting things 
         this.restitution = 1;
+        this.dragCoeff = 0.47f; // 0.47 for a sphere according to wikipedia
 
         // Adjusting the scale
         this.shownTransform.localScale = new Vector2(this.radius*2, this.radius*2);
@@ -165,33 +239,43 @@ class particle {
     // Methods
 
     // An update method that changes the location based on the velocity at that time
-    public void updateLocation() {
+    public void update() {
 
         // Adding to the velocity based on the acceleration
         // Using vf = v0 + a*t
         this.velocity = this.velocity + this.accel * Time.deltaTime;
 
-        // Resetting the acceleration now that we have accounted for it
-        this.accel = Constants.g;
+        // Resetting the acceleration to gravity
+        this.accel = Vector2.zero;
+        this.force(Constants.g * this.mass);
 
         // Updating the location adjusted for the change in time
         this.shownTransform.position += new Vector3(this.velocity.x * Time.deltaTime, this.velocity.y * Time.deltaTime, 0);
 
         // Checking the bounce to make sure it isn't going off the screen
-        this.checkBounces();
+        this.checkWalls();
 
     }
 
     // A method that applies a force to the particle
     public void force(Vector2 f) {
 
+        // We actually have to account for air resistance here
+        // We need to subtract the drag force from the inputted force.
+        // There comes a force when the drag force is higher than the inputted force
+        // Which means it is either at terminal velocity or higher
+        // Drag Force = mass * DragCoeff. * (AirDensity)/2 * velocity^2 * area 
+        // These are all spheres, so they would be pi * r^2 
+        // Then we have to normalize it to the opposite direction of the velocity
+        Vector2 dragForce = this.mass * this.dragCoeff * Constants.AIR_DENSITY / 2 * this.velocity.magnitude * this.velocity.magnitude * this.radius * this.radius * PI * -this.velocity.normalized;  
+
         // Using F=ma, we can find the acceleration due to the force
-        this.accel += f  * (float)(1.0 / this.mass);
+        this.accel += (f + dragForce)  * (float)(1.0 / this.mass);
 
     }
 
     // A method that checks for bounces on the edge of the screen
-    public void checkBounces() {
+    public void checkWalls() {
 
         // Checking the bottom of the screen
         if (this.shownTransform.position.y - this.radius< -Constants.stageDimensions.y) {
@@ -248,13 +332,22 @@ class particle {
 
     }
 
-    // Collision Detection
+    // A method to return the velocity as a 3D vector instead of a 2D vector
+    public Vector3 velocity3D() {
+
+        return new Vector3(this.velocity.x, this.velocity.y, 0);
+
+    }
+
+    // ! Deprecated! 
+    // Collision Detection 
     public static void fixCollisions(particle[] particles){
 
         // Looping for all of them to see which ones are close
         for (int i = 0; i < particles.Length; i++) {
 
-            for (int j = 0; j < particles.Length; j++) {
+            // Start j from i + 1 to avoid redundant checks
+            for (int j = i + 1; j < particles.Length; j++) {
 
                 // Making sure that it doesn't try to check with itself
                 if (i == j) continue;
@@ -262,33 +355,19 @@ class particle {
                 // We don't want to do square root because it is very slow, so we are 
                 // going to check to see if the distance between them is less than the
                 // (2 * radius) squared 
-                float dist = Vector3.Distance(particles[i].shownTransform.position, particles[j].shownTransform.position);
-                // pow((particles[i].shownTransform.position.x - particles[j].shownTransform.position.x), 2) + pow((particles[j].shownTransform.position.x - particles[j].shownTransform.position.y), 2);
+                float distSquared = (particles[i].shownTransform.position - particles[j].shownTransform.position).sqrMagnitude;
 
-                if (dist <= (particles[i].radius + particles[i].radius)) {
-
-                    // Now we have to solve the issue.
-                    // So, to solve them, I think I need to find the line that connects the centers
-                    // of the circle and then fix them by reflecting their velocities across the line. 
-
-                    // Then I just need to use conservation of momentum to figure out the velocity. But then also impulse 👍
-                    // Shouldn't be too hard, right?
-
-
-                    // Right????
+                if (distSquared <= (particles[i].radius + particles[j].radius) * (particles[i].radius + particles[j].radius)) {
 
                     // First off, finding the normal
-                    Vector3 normal = particles[i].shownTransform.position - particles[j].shownTransform.position;
+                    Vector3 normal3 = particles[i].shownTransform.position - particles[j].shownTransform.position;
+                    Vector2 normal = new Vector2(normal3.x, normal3.y).normalized;
 
-                    // A reference to the normal as a vector2
-                    Vector2 flatNormal = new Vector2(normal.x, normal.y);
+                    // Finding the relative velocity
+                    Vector2 relativeVelocity = particles[i].velocity - particles[j].velocity;
 
-                    if (Vector2.Dot(particles[i].velocity - particles[j].velocity, flatNormal) > 0) continue; // Skip if they are moving apart
-
-                    // I need to use the reflection formula for both to find the new velocities
-                    // new = vector - 2 * (vector dot normal) * normal
-                    particles[i].velocity = particles[i].velocity - 2 * (Vector2.Dot(particles[i].velocity, flatNormal.normalized)) * flatNormal.normalized;
-                    particles[j].velocity = particles[j].velocity - 2 * (Vector2.Dot(particles[j].velocity, flatNormal.normalized)) * flatNormal.normalized;
+                    // Skipping if they are moving apart 
+                    if (Vector2.Dot(relativeVelocity, normal) >= 0) continue;
 
                     // Finding the coefficient of restitution that we will use
                     // The minimum because the one with the least restitution will deform slightly
@@ -296,23 +375,26 @@ class particle {
                     float restitution = Mathf.Min(particles[j].restitution, particles[i].restitution);
 
                     // Finding the impulse, J = change in momentum
-                    float impulseScalar = -(1 + restitution) * Vector3.Dot(particles[i].velocity - particles[j].velocity, normal);;
-                    impulseScalar /= 1 / particles[i].mass + 1 / particles[j].mass;
+                    float impulseScalar = -(1 + restitution) * Vector2.Dot(relativeVelocity, normal);
+                    impulseScalar /= (1 / particles[i].mass + 1 / particles[j].mass);
 
                     // Apply impulse to the particles
-                    Vector3 impulse = impulseScalar * normal;
-                    Vector2 flatImpulse = new Vector2(impulse.x, impulse.y);
-                    particles[i].velocity -= flatImpulse / particles[i].mass;
-                    particles[j].velocity += flatImpulse / particles[j].mass;
+                    Vector2 impulse = impulseScalar * normal;
+                    particles[i].velocity += impulse / particles[i].mass;
+                    particles[j].velocity -= impulse / particles[j].mass;
 
-                    // Now to also move them out of each other
-                    // It is just along the line that they collided
+                    // TODO See if you can get rid of this square root
+                    // Finding out how much we need to move it (annoyingly this means we need the square root)
+                    float overlapDepth = particles[i].radius + particles[j].radius - Mathf.Sqrt(distSquared);
+
+                    // Finding the correction vector and accounting for the mass of each to make it a bit smoother
+                    Vector2 correction = normal * Mathf.Max(overlapDepth, 0)      / (particles[i].mass + particles[j].mass);
 
                     // Moving the first one
-                    particles[i].shownTransform.position = particles[i].shownTransform.position - normal.normalized * (normal.magnitude - particles[i].radius - particles[j].radius);
+                    particles[i].shownTransform.position -= new Vector3(correction.x, correction.y, 0) * particles[j].mass;
 
                     // Moving the second one
-                    particles[j].shownTransform.position = particles[j].shownTransform.position + normal.normalized * (normal.magnitude - particles[j].radius - particles[i].radius);
+                    particles[j].shownTransform.position += new Vector3(correction.x, correction.y, 0) * particles[i].mass;
 
                 }
 
@@ -320,6 +402,41 @@ class particle {
         }
 
     } 
+
+    // Gravity :D
+    public static void addGravity(particle focus, particle[] particles) {
+
+        // Summing the forces 
+        Vector2 f = Vector2.zero;
+
+        // Looping through each of the other particles and adding their gravity to the focus
+        for (int i = 0; i < particles.Length; i++) {
+
+            // The force of gravity due to each one is 
+            // F = G * M1 * M2 / r^2
+
+            // First off, finding the normal
+            Vector3 normal = focus.shownTransform.position - particles[i].shownTransform.position;
+
+            // A reference to the normal as a vector2
+            Vector2 flatNormal = new Vector2(normal.x, normal.y);
+
+            // Actually calculating the force
+            // Making sure that there is no divide by zero
+            float dist = Vector2.Distance(focus.shownTransform.position, particles[i].shownTransform.position);
+            if (dist != 0) {
+                Vector2 forceG = -Constants.G * flatNormal.normalized * focus.mass * particles[i].mass / pow(dist, 2);
+
+                // Adding the force to the total force
+                f += forceG;
+            }
+
+        }
+
+        // Applying the force
+        focus.force(f);
+
+    }
 
 }
 
